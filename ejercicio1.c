675f6c386f9f37896870f9611da142a53d86ac53
carlosmartinezac <carlosmartinezac2007@gmail.com>
2025-10-29 09:42:09 -0500

    Update ejercicio1.c

--- ejercicio1.c
+++ ejercicio1.c
@@ -2,14 +2,21 @@
 
 int main (){
 
+    //Variables
     int numero, doble, triple;
 
+    //Datos de entrada
     printf ("Escriba el numero entero \n");
     scanf ("%i", &numero);
+
+    //Proceso
     doble = 2 * numero;
     triple = 3 * numero;
+
+    //Datos de salida
     printf ("El doble de su numero es: %i y el triple de su numero es: %i",doble, triple);
 
     return 0;
+
 
 }
